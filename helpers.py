e2452bdc7faefa00922a9ee1232a00e8efe86486
swynnejr <saucersam@gmail.com>
2021-07-11 18:14:14 -0500

    All three done

---
+++ helpers.py
@@ -0,0 +1,15 @@
+def generate_checkerboard(x, y):
+    result = []
+
+    for j in range (0, y):
+        temp = []
+        for i in range(0, x):
+            temp.append((i + j) % 2)
+        result.append(temp)
+
+    return result
+
+result = generate_checkerboard(12,12)
+
+# for row in result:
+#     print(row)
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from helpers import generate_checkerboard
+app = Flask(__name__)
+
+@app.route('/')
+def checkerboard_1():
+    result = generate_checkerboard(8, 8)
+    return render_template("checkerboard.html", result = result)
+
+#adding int: to <int:times> is the same as line 12
+@app.route('/<int:y>')
+def checkerboard_2(y):
+    result = generate_checkerboard(8, y)
+    return render_template("checkerboard.html", result = result)
+
+@app.route('/<int:x>/<int:y>')
+def checkerboard_3(x, y):
+    result = generate_checkerboard(x, y)
+    return render_template("checkerboard.html", result = result)
+    # times = int(times)
+    # return render_template("index.html", times = times, color = color)
+
+if __name__=="__main__":
+    app.run(debug=True)
